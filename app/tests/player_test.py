29e2df726a22c25b3175b354e0820e89dc199fc6
Laura Dobie <moving.toyshop@gmail.com>
2020-11-20 16:00:36 +0000

    Player has choice

--- app/tests/player_test.py
+++ app/tests/player_test.py
@@ -7,3 +7,9 @@ class TestPlayer(unittest.TestCase):
         player1 = Player("player 1", "rock")
 
         self.assertEqual("player 1", player1.name)
+
+    def test_player_has_choice(self):
+        player1 = Player("player 1", "rock")
+
+        self.assertEqual("rock", player1.choice)
+
