c5340a889e57a54753c1c55468237135190090a9
Laura Dobie <moving.toyshop@gmail.com>
2020-11-20 17:48:50 +0000

    Scissors beat paper

--- app/models/game.py
+++ app/models/game.py
@@ -4,22 +4,22 @@ class Game():
     def __init__(self, name):
         self.name = name
 
-    # def play_rock_paper_scissors(self, player_1, player_2):
-    #     if player_1.choice == "paper" and player_2.choice == "rock":
-    #         return player_1
-    #     elif player_2.choice == "paper" and player_1.choice == "rock"
-    #         return player_2
+    def play_rock_paper_scissors(self, player_1, player_2):
+        if player_1.choice == "paper" and player_2.choice == "rock":
+            return player_1
+        elif player_2.choice == "paper" and player_1.choice == "rock":
+            return player_2
 
-    #     elif player_1.choice == "scissors" and player_2.choice == "paper":
-    #         return player_1
-    #     elif player_2.choice == "scissors" and player_1.choice == "paper"
-    #         return player_2
+        elif player_1.choice == "scissors" and player_2.choice == "paper":
+            return player_1
+        elif player_2.choice == "scissors" and player_1.choice == "paper":
+            return player_2
 
-    #     elif player_1.choice == "rock" and player_2.choice == "scissors":
-    #         return player_1
-    #     elif player_2.choice == "rock" and player_1.choice == "scissors"
-    #         return player_2
+        elif player_1.choice == "rock" and player_2.choice == "scissors":
+            return player_1
+        elif player_2.choice == "rock" and player_1.choice == "scissors":
+            return player_2
 
-    #     elif player_1.choice == player_2.choice:
-    #         return None
+        elif player_1.choice == player_2.choice:
+            return None
 
--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -6,14 +6,23 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.player_1 = Player("Dave", "scissors")
         self.player_2 = Player("Laura", "paper")
+        self.player_3 = Player("Chiara", "rock")
         
         self.game = Game("Rock Paper Scissors")
 
     def test_game_has_name(self):
         self.assertEqual("Rock Paper Scissors", self.game.name)
 
-    # def test_scissors_beat_paper(self):
-    #     self.game.rock_paper_scissors(self.player_1, self.player_2):
+    def test_scissors_beat_paper(self):
+        result = self.game.play_rock_paper_scissors(self.player_1, self.player_2)
+
+        self.assertEqual(self.player_1, result)
+
+    # def test_rock_beats_scissors(self):
+    #     result = self.game.play_rock_paper_scissors(self.player_1, self.player_3)
+
+    #     self.assertEqual(self.player_3, result)
+
 
 
         
