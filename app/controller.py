3815ec5c3d61a3f533875cd01a8e94f336426150
Laura Dobie <moving.toyshop@gmail.com>
2020-11-21 10:17:11 +0000

    MVP works and randomisation works

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect
 from app import app
 from app.models.player import Player
 from app.models.game import *
+from random import choice
 
 @app.route('/')
 def test():
@@ -24,3 +25,38 @@ def get_result():
         result = "It's a draw!"
 
     return render_template('result.html', choice1=player_1.choice, choice2=player_2.choice, player1name=player_1.name, player2name=player_2.name, title='Result', result=result)
+
+@app.route('/<choice1>/<choice2>')
+def play_game(choice1, choice2):
+    player_1 = Player("Owl", choice1)
+    player_2 = Player("Pussycat", choice2)
+
+    current_game = Game(player_1, player_2)
+
+    winner = current_game.play_rock_paper_scissors()
+
+    if winner != None:
+        result = f"{winner.name} is the winner!"
+    else: 
+        result = "It's a draw!"
+
+    return render_template('play.html', choice1=choice1, choice2=choice2, player1name=player_1.name, player2name=player_2.name, title='Result', result=result)
+
+@app.route('/computer')
+def play_computer():
+    weapons = ["rock", "paper", "scissors"]
+    random_weapon = choice(weapons)
+
+    player_1 = Player("Owl", "scissors")
+    player_2 = Player("Cyber Pussycat", random_weapon)
+
+    current_game = Game(player_1, player_2)
+
+    winner = current_game.play_rock_paper_scissors()
+
+    if winner != None:
+        result = f"{winner.name} is the winner!"
+    else: 
+        result = "It's a draw!"
+
+    return render_template('play-computer.html', choice1=player_1.choice, choice2=player_2.choice, player1name=player_1.name, player2name=player_2.name, title='Result', result=result)        
